1f0ed177bfbc6057f9dfd740f42624583668158a
Taheera Ahmed <60318348+taheeraahmed@users.noreply.github.com>
2023-03-21 11:39:58 +0100

    Testing commits changed main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
 #include <Arduino.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7735.h>
+#include <SPI.h>
 
 const int LED_PIN = 44;
+const int TFT_CS = ???;
+const int TFT_DC = ???;
+const int TFT_MOSI = ???;
+const int TFT_SCLK = ???;
+const int TFT_RST = ???;
 
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_PIN, OUTPUT);
+  Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
 }
 
 // the loop function runs over and over again forever
