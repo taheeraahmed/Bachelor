cdcb5e6488f30f254f03ec688a730ecfc29194b9
Taheera Ahmed <60318348+taheeraahmed@users.noreply.github.com>
2023-03-21 14:02:34 +0100

    Writing to screen

--- src/main.cpp
+++ src/main.cpp
@@ -10,14 +10,25 @@ const int TFT_MOSI = 24;
 const int TFT_SCLK = 23;
 const int TFT_RST = 26;
 
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
+
+void testdrawtext(char *text, uint16_t color) {
+  tft.setCursor(0, 0);
+  tft.setTextColor(color);
+  tft.setTextWrap(true);
+  tft.print(text);
+}
+
 void setup() {
-  Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
   tft.initR(INITR_BLACKTAB);   // initialize a ST7735S chip, black tab
   uint16_t time = millis();
   tft.fillScreen(ST7735_BLACK);
   time = millis() - time;
   // large block of text
   tft.fillScreen(ST7735_WHITE);
+
+  testdrawtext("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur adipiscing ante sed nibh tincidunt feugiat. Maecenas enim massa, fringilla sed malesuada et, malesuada sit amet turpis. Sed porttitor neque ut ante pretium vitae malesuada nunc bibendum. Nullam aliquet ultrices massa eu hendrerit. Ut sed nisi lorem. In vestibulum purus a tortor imperdiet posuere. ", ST7735_BLACK);
+  delay(1000);
   // testdrawtext("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur adipiscing ante sed nibh tincidunt feugiat. Maecenas enim massa, fringilla sed malesuada et, malesuada sit amet turpis. Sed porttitor neque ut ante pretium vitae malesuada nunc bibendum. Nullam aliquet ultrices massa eu hendrerit. Ut sed nisi lorem. In vestibulum purus a tortor imperdiet posuere. ", ST77XX_WHITE);
   delay(1000);
 }
@@ -29,3 +40,4 @@ void loop() {
   digitalWrite(LED_PIN, LOW);   // turn the LED off by making the voltage LOW
   delay(1000);                  // wait for a second
 }
+
