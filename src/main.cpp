fefabe17c4e6d9ddd460a8e8b717ce09270d2751
minfyk <97945430+minfyk@users.noreply.github.com>
2023-04-07 18:18:00 +0200

    Customized Keypad read keys

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,34 @@
 #include <Arduino.h>
+#include <Keypad.h>
 
-const int LED_PIN = 44;
+                                          // Definerer antall rader og kolonner
+const byte ROWS = 4;                      //fire rader
+const byte COLS = 3;                      //tre kolonner
 
-void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_PIN, OUTPUT);
+// Definerer symbolene tilhørende de forskjellige knappene på tastaturet
+char hexaKeys[ROWS][COLS] = {
+  {'1','2','3'},
+  {'4','5','6'},
+  {'7','8','9'},
+  {'*','0','#'}
+};
+// 
+byte rowPins[ROWS] = {22, 23, 24, 25};      //connect to the row pinouts of the keypad
+byte colPins[COLS] = {26, 27, 28};          //connect to the column pinouts of the keypad
+
+//initialize an instance of class NewKeypad
+// Keypad(makeKeymap(userKeymap), row[], col[], rows, cols)
+
+Keypad this_keypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
+
+void setup(){
+  Serial.begin(9600);
 }
-
-// the loop function runs over and over again forever
-void loop() {
-  digitalWrite(LED_PIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(1000);                  // wait for a second
-  digitalWrite(LED_PIN, LOW);   // turn the LED off by making the voltage LOW
-  delay(1000);                  // wait for a second
+  
+void loop(){
+  char customKey = this_keypad.getKey();
+  
+  if (customKey){
+    Serial.println(customKey);
+  }
 }
