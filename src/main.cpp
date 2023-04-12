3c40ce7534feba69be740f05d6a95c2ca707bca9
Taheera Ahmed <60318348+taheeraahmed@users.noreply.github.com>
2023-04-12 14:43:44 +0200

    shoot me

--- src/main.cpp
+++ src/main.cpp
@@ -17,38 +17,9 @@ int  fanSpeed;
 void setup() {
   // put your setup code here, to run once:
   pinMode(3, OUTPUT);
-  EEPROM.get(FS_ADDR, fanSpeed);
-  if(fanSpeed < 1)  fanSpeed = 255;
-  analogWrite(FAN_PIN, fanSpeed);
-  Serial.begin(9600);
 
 }
-char  rx_byte = 0;
-String input = "";
 
 void loop() {
-  if (Serial.available()  > 0) {    // is a character available?
-    rx_byte = Serial.read();       //  get the character
   
-    // check if a number was received
-    if ((rx_byte  >= '0') && (rx_byte <= '9')) {
-      input.concat(rx_byte);
-      
-    }
-    else if (rx_byte == '\n') {
-      Serial.print("Received: ");
-      Serial.println(input);
-      if(input.toInt() < 256) {
-        fanSpeed = input.toInt();
-        EEPROM.put(FS_ADDR,  fanSpeed);
-      } else {
-        Serial.println("Invalid Number");
-      }
-      input = "";
-    }
-    else {
-      Serial.println("Not  a number.");
-    }
-  } // end: if (Serial.available() > 0)
-  analogWrite(FAN_PIN,  fanSpeed);
 }
