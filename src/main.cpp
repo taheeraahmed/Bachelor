8e42e393af431f52d0040f246487387fe9a0aca2
Taheera Ahmed <60318348+taheeraahmed@users.noreply.github.com>
2023-04-12 11:17:46 +0200

    Minor changes

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,11 @@
 #include <utils.h>
 
 MEMORY_EXTENSION_PINS memExtPins;
-// TODO: Set temperature pins (Elise)
 TEMPERATURE_PINS tempPins;
-// TODO: Define the test choices  (Mina)
 TestChoices choices;
 String fileName;
 
 void setup() {
-  // TODO: Set startTime (Mina)
   unsigned long startTime;
   fileName = setupReadCSV(memExtPins, startTime, choices);
 }
--- src/save_temp_to_csv.h
+++ src/save_temp_to_csv.h
@@ -1,5 +1,6 @@
 #include <SPI.h>
 #include <SD.h>
+#include <utils.h>
 
 // Define the test choices
 
