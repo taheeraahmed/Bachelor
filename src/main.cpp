b34db7103e85efc0d8b9268f54b52e1cb02dc2be
Taheera Ahmed <60318348+taheeraahmed@users.noreply.github.com>
2023-04-13 11:23:47 +0200

    Fan is going WOOOM

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,25 @@
 #include <Arduino.h>
 #include <utils.h>
 #include <fan.h>
-#include <EEPROM.h>
-
-const int LED_PIN = 44;
 
 MEMORY_EXTENSION_PINS mem_ext_pins;
 TEMPERATURE_PINS temp_pins;
-//FAN_PINS fan_pins;
 TestChoices choices;
 
-const int FAN_PIN = 3;
-#define FS_ADDR 0x01
-int  fanSpeed;
+const int FAN_PIN = 13;
+int i = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(3, OUTPUT);
-
+  pinMode(FAN_PIN, OUTPUT);
 }
 
 void loop() {
-  
+  int speed = 180;
+  analogWrite(FAN_PIN, speed);
+  delay(3000);
+
+  speed = 0;
+  i = i + 1;
+  analogWrite(FAN_PIN, speed);
+  delay(3000);
 }
