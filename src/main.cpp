b1bc7b0bdabce9d800281cd2a7edf28075210e50
Taheera Ahmed <60318348+taheeraahmed@users.noreply.github.com>
2023-04-14 12:52:34 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,11 @@ MEMORY_EXTENSION_PINS mem_ext_pins;
 TEMPERATURE_PINS temp_pins;
 FAN_PINS fan_pins;
 TestChoices choices;
-String filename;
-
-unsigned long start_time = millis();
 
 void setup() {
-  filename = setupReadCSV(mem_ext_pins, start_time, choices);
   pinMode(fan_pins.CONTROL, OUTPUT);
   pinMode(fan_pins.LED, OUTPUT);
 }
 
 void loop() {
-  readSensorValuesToCsv(temp_pins, filename.c_str());
 }
