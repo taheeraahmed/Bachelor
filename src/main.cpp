1853d6599eba9747d72e73c7efa9f21c667ce877
Elise Havmo <aehavmo@stud.ntnu.no>
2023-04-21 12:31:54 +0200

    Added functionality for temperature sensors

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,19 @@
+#include <avr/io.h>
+#include <util/delay.h>
 #include <Arduino.h>
+#include <Temp/Temp.h>
 
-const int LED_PIN = 44;
+int main(void)
+{
+  Serial.begin(9600);
+  initADC();
+  initPort();
+  calcLedID();
 
-void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_PIN, OUTPUT);
-}
-
-// the loop function runs over and over again forever
-void loop() {
-  digitalWrite(LED_PIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(1000);                  // wait for a second
-  digitalWrite(LED_PIN, LOW);   // turn the LED off by making the voltage LOW
-  delay(1000);                  // wait for a second
+  while (1)
+  {
+   printADC();
+   _delay_ms(2000);
+  }
+  
 }
