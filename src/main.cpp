c4ac7c02d32ec03cd701fc1f6b41260969437f3e
Taheera Ahmed <60318348+taheeraahmed@users.noreply.github.com>
2023-04-27 11:27:33 +0200

    Changed minor things

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,34 @@
-#include <Arduino.h>
-#include <save_temp_to_csv.h>
-#include <utils.h>
-#include <fan.h>
+#include <SD.h>
 
-MEMORY_EXTENSION_PINS mem_ext_pins;
-TEMPERATURE_PINS temp_pins;
-FAN_PINS fan_pins;
-TestChoices choices;
-String filename;
+File myFile;
 
-unsigned long start_time = millis();
-
+/* 
+- MOSI (Master Out Slave In): connect to Arduino Mega pin 51
+- MISO (Master In Slave Out): connect to Arduino Mega pin 50
+- SCK (Serial Clock): connect to Arduino Mega pin 52
+- CS (Chip Select): you can use any available digital pin on the Arduino Mega, but for this example, let's use pin 53. Connect CS to Arduino Mega pin 53.
+ */
 void setup() {
-  filename = setupReadCSV(mem_ext_pins, start_time, choices);
-  pinMode(fan_pins.CONTROL, OUTPUT);
-  pinMode(fan_pins.LED, OUTPUT);
+  Serial.begin(9600);
+  pinMode(53, OUTPUT); // Set CS pin as an output
+  digitalWrite(53, HIGH); // Deselect the SD card
+  
+  //Initialize SD card
+  if (!SD.begin(53)) { // Use pin 53 as the CS pin
+    Serial.println("SD card initialization failed!");
+    return;
+  }
+  Serial.println("SD card initialization successful.");
+  
+  //Open file
+  myFile = SD.open("test.txt", FILE_WRITE);
+  
+  //Write to file
+  myFile.println("Hello world!");
+  
+  //Close file
+  myFile.close();
 }
 
 void loop() {
-  readSensorValuesToCsv(temp_pins, filename.c_str());
-  //turnOnFans(fan_pins);
 }
