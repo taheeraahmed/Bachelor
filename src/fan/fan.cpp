9211d0b5d40b8e87e803547bcc6fd349d04211d9
Taheera Ahmed <60318348+taheeraahmed@users.noreply.github.com>
2023-04-27 11:31:16 +0200

    Updated with code Vilma wrote

--- src/fan/fan.cpp
+++ src/fan/fan.cpp
@@ -1,9 +1,12 @@
-void turnOnFans(FAN_PINS fan_pins) {
-    analogWrite(fan_pins.CONTROL, 255);
-    analogWrite(fan_pins.LED, 255);
-}
+#include <Arduino.h>
 
-void turnOffFans(FAN_PINS fan_pins) {
-    analogWrite(fan_pins.CONTROL, 0);
-    analogWrite(fan_pins.LED, 0);
+void initFans(){
+    // Setter pinnene til viftene som Output.
+    DDRB |= (1 << PIN5) | (1 << PIN4);
+};
+void turnOnFans() {
+    PORTB |= (1 << PIN5) | (1 << PIN4);
+}
+void turnOffFans() {
+    PORTB &= ~((1 << PIN5) | (1 << PIN4));
 }
--- src/fan/fan.h
+++ src/fan/fan.h
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 #include <utils.h>
-#include <fan.cpp>
+#include "fan.cpp"
 
 void turnOnFans(FAN_PINS fan_pins);
 void turnOffFans(FAN_PINS fan_pins);
