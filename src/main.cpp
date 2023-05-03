7169054cc328addc788956507eeb404586a294f0
minfyk <97945430+minfyk@users.noreply.github.com>
2023-05-03 14:38:48 +0200

    Fixed keypad

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,58 @@
+#include <avr/io.h>
+#include <util/delay.h>
 #include <Arduino.h>
 
-const int LED_PIN = 44;
+#define used_pins_keypad 0b01111111
+#define row_pins 0b00001111
+#define col_pins 0b01110000
+#define Keypad_DDRx DDRA
+#define Keypad_PORTx PORTA
+#define Keypad_PINx PINA
+
+/*void init_keypad() {
+  Keypad_DDRx &= ~(used_pins_keypad & row_pins); //Setter alle rad pinnene til innsignal og resten til utsignal
+  Keypad_DDRx |= (used_pins_keypad & col_pins); // Setter alle kolonne pinnene til utsignal Setter 
+  Keypad_PORTx |= (used_pins_keypad & row_pins); //Enabler pull up resistor på alle rad pinnene
+}*/
 
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_PIN, OUTPUT);
+//init_keypad();
+Serial.begin(9600);
+
+Keypad_DDRx  |= (1 << 6) | (1 << 5) | (1 << 4);       //kobler kolonnepinnene på A 
+Keypad_PORTx |= (1 << 6) | (1 << 5) | (1 << 4);       //kolonnepinnene på PORTA
+Keypad_PORTx |= (1 << 0) | (1 << 1) | (1 << 2) | (1 << 3);  //Radpinnene på PORTA
+/*
+DDRA  |= (1 << 2) | (1 << 4) | (1 << 6);
+PORTA |= (1 << 2) | (1 << 4) | (1 << 6);
+PORTC |= (1 << 1) | (1 << 3) | (1 << 5) | (1 << 7);*/
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_PIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(1000);                  // wait for a second
-  digitalWrite(LED_PIN, LOW);   // turn the LED off by making the voltage LOW
-  delay(1000);                  // wait for a second
+  PORTA &= ~(1 << 6);
+  delay(1);
+  if(!(PINA & (1 << 3))){ Serial.print("#\n"); }
+  if(!(PINA & (1 << 2))){ Serial.print("9\n"); }
+  if(!(PINA & (1 << 1))){ Serial.print("6\n"); }
+  if(!(PINA & (1 << 0))){ Serial.print("3\n"); }
+  PORTA |= (1 << 6);
+
+  PORTA &= ~(1 << 5);
+  delay(1);
+  if(!(PINA & (1 << 3))){ Serial.print("0\n"); }
+  if(!(PINA & (1 << 2))){ Serial.print("8\n"); }
+  if(!(PINA & (1 << 1))){ Serial.print("5\n"); }
+  if(!(PINA & (1 << 0))){ Serial.print("2\n"); }
+  PORTA |= (1 << 5);
+
+  PORTA &= ~(1 << 4);
+  delay(1);
+  if(!(PINA & (1 << 3))){ Serial.print("*\n"); }
+  if(!(PINA & (1 << 2))){ Serial.print("7\n"); }
+  if(!(PINA & (1 << 1))){ Serial.print("4\n"); }
+  if(!(PINA & (1 << 0))){ Serial.print("1\n"); }
+  PORTA |= (1 << 4);
+
+  delay(150);
+
 }
