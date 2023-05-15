bc8bb28d399e8bd528283edf41a16fe683a7213c
Taheera Ahmed <60318348+taheeraahmed@users.noreply.github.com>
2023-05-15 14:20:42 +0200

    Fixed error in header file

--- src/writeToFile/writeToFile.h
+++ src/writeToFile/writeToFile.h
@@ -1,6 +1,6 @@
 #include <SD.h>
 #include <stdio.h>
 
-uint8_t createFile(char *headers, uint8_t start_time, bool is_error);
+uint8_t createFile(char *headers, uint8_t start_time, bool is_error, uint8_t patient_id);
 uint8_t writeToFile(char *filename, const char *data);
 char *convertDataToChar(uint8_t temp_pcb, uint8_t temp_air, uint8_t temp_skin, uint8_t temp_led, const char *datetime);
