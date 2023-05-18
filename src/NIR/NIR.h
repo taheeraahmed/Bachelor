efdd1a91a08a7555371f12a41dd6bade43b4634e
villa135 <vilmaas@stud.ntnu.no>
2023-05-18 13:28:14 +0200

    Fiksing på .h filer

--- src/NIR/NIR.h
+++ src/NIR/NIR.h
@@ -4,12 +4,7 @@
 #ifndef NIR_H
 #define NIR_H
 
-
-
 void initTimer0();
 void initNIR(uint8_t NIRDuty, uint8_t R_ID);
-
-extern "C" void TIMER0_COMPA_vect (void);
-extern "C" void TIMER0_OVF_vect (void);  
 
 #endif
