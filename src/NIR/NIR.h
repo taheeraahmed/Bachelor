2734b18e544a69fe414e51b0e00a280ba887d9a7
villa135 <vilmaas@stud.ntnu.no>
2023-05-22 20:49:05 +0200

    Ferdig kommentering av NIR lys

--- src/NIR/NIR.h
+++ src/NIR/NIR.h
@@ -1,10 +1,13 @@
 #include <avr/interrupt.h>
 #include <avr/io.h>
+#include "TWI/TWI.h"
 
 #ifndef NIR_H
 #define NIR_H
 
+// Funksjons dekalrasjoner for kontroll av NIR-lys.
 void initTimer0();
 void initNIR(uint8_t NIRDuty, uint8_t R_ID);
+void endNIR(void);
 
 #endif
