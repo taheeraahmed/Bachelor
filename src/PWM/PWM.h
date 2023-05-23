6c0c70c7b58734ba3f7b0839d1cc9482b55f7810
villa135 <vilmaas@stud.ntnu.no>
2023-05-23 11:05:50 +0200

    Småfiks ikke helt ferdig

--- src/PWM/PWM.h
+++ src/PWM/PWM.h
@@ -4,6 +4,14 @@
 #include <stdbool.h>
 #include <getTime/getTime.h>
 
+
+#ifndef PWM_H
+#define PWM_H
+
+// Funksjonsdeklarasjon for funksjoner som bruker PWM på timer3.
 void initTimer3(void);
 void setBuzzerAlarm(bool buzzerState, uint8_t buzzDuty, uint16_t interval);
 void setFans(bool controllFans, bool ledFans, uint8_t controllFanDuty, uint8_t ledFanDuty);
+
+#endif // !PWM_H
+
